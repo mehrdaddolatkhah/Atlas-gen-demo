50924d8a9cf4efa67f1a89b25c09afd8b389ad44
Mehrdad <dolatkhah.mehrdad@gmail.com>
2020-06-02 23:25:31 +0430

    update ui - image and font in login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:atlas_gen_demo/Animation/FadeAnimation.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -11,147 +12,141 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            Container(
-              height: 400,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/images/background.png'),
-                  fit: BoxFit.fill,
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 400,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/login.png'),
+                    fit: BoxFit.fill,
+                  ),
                 ),
               ),
-              child: Stack(
-                children: <Widget>[
-                  Positioned(
-                    left: 30,
-                    width: 80,
-                    height: 200,
-                    child: Container(
-                      decoration: BoxDecoration(
-                          image: DecorationImage(
-                              image: AssetImage('assets/images/light-1.png'))),
-                    ),
-                  ),
-                  Positioned(
-                    left: 140,
-                    width: 80,
-                    height: 150,
-                    child: Container(
-                      decoration: BoxDecoration(
-                          image: DecorationImage(
-                              image: AssetImage('assets/images/light-2.png'))),
-                    ),
-                  ),
-                  Positioned(
-                    right: 40,
-                    top: 40,
-                    width: 80,
-                    height: 150,
-                    child: Container(
-                      decoration: BoxDecoration(
-                          image: DecorationImage(
-                              image: AssetImage('assets/images/clock.png'))),
-                    ),
-                  ),
-                  Positioned(
-                    child: Container(
-                      margin: EdgeInsets.only(top: 50),
+              Positioned(
+                child: FadeAnimation(
+                    1.8,
+                    Container(
+                      margin: EdgeInsets.only(top: 10),
                       child: Center(
                         child: Text(
-                          "Login",
+                          "ورود به برنامه",
+                          textAlign: TextAlign.center,
                           style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 40,
-                              fontWeight: FontWeight.bold),
+                            color: Color.fromRGBO(143, 148, 251, 1),
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'persianBold',
+                          ),
                         ),
                       ),
-                    ),
-                  )
-                ],
+                    )),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(30.0),
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    padding: EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: [
-                        BoxShadow(
-                            color: Color.fromRGBO(143, 148, 251, .2),
-                            blurRadius: 20.0,
-                            offset: Offset(0, 10))
-                      ],
-                    ),
-                    child: Column(
-                      children: <Widget>[
-                        Container(
-                          padding: EdgeInsets.all(8.0),
-                          decoration: BoxDecoration(
-                              border: Border(
-                            bottom: BorderSide(color: Colors.grey[100]),
-                          )),
-                          child: TextField(
-                            decoration: InputDecoration(
-                              border: InputBorder.none,
-                              hintText: "Email or Phone number",
-                              hintStyle: TextStyle(color: Colors.grey[400]),
+              Padding(
+                padding: EdgeInsets.all(30.0),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Color.fromRGBO(143, 148, 251, .2),
+                              blurRadius: 20.0,
+                              offset: Offset(0, 10))
+                        ],
+                      ),
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            padding: EdgeInsets.all(8.0),
+                            decoration: BoxDecoration(
+                                border: Border(
+                              bottom: BorderSide(color: Colors.grey[100]),
+                            )),
+                            child: TextField(
+                              textDirection: TextDirection.rtl,
+                              textAlign: TextAlign.right,
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: "09123456789",
+                                hintStyle: TextStyle(
+                                  color: Colors.grey[400],
+                                  fontFamily: 'persianMedium',
+                                  fontSize: 14,
+                                ),
+                              ),
                             ),
                           ),
-                        ),
-                        Container(
-                          padding: EdgeInsets.all(8.0),
-                          child: TextField(
-                            decoration: InputDecoration(
-                              border: InputBorder.none,
-                              hintText: "Password",
-                              hintStyle: TextStyle(color: Colors.grey[400]),
+                          Container(
+                            padding: EdgeInsets.all(8.0),
+                            child: TextField(
+                              textAlign: TextAlign.right,
+                              textDirection: TextDirection.rtl,
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: "کلمه عبور",
+                                hintStyle: TextStyle(
+                                  color: Colors.grey[400],
+                                  fontFamily: 'persianMedium',
+                                  fontSize: 14,
+                                ),
+                              ),
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Container(
-                    height: 50,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      gradient: LinearGradient(
-                        colors: [
-                          Color.fromRGBO(143, 148, 251, .1),
-                          Color.fromRGBO(143, 148, 251, .6),
                         ],
                       ),
                     ),
-                    child: Center(
-                      child: Text(
-                        "Login",
-                        style: TextStyle(
-                            color: Colors.white, fontWeight: FontWeight.bold),
-                      ),
+                    SizedBox(
+                      height: 30,
                     ),
-                  ),
-                  SizedBox(
-                    height: 70,
-                  ),
-                  Text(
-                    "Forget Password",
-                    style: TextStyle(
-                      color: Color.fromRGBO(143, 148, 251, .6),
+                    FadeAnimation(
+                        2,
+                        Container(
+                          height: 50,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            gradient: LinearGradient(
+                              colors: [
+                                Color.fromRGBO(143, 148, 251, .4),
+                                Color.fromRGBO(143, 148, 251, .8),
+                              ],
+                            ),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "ورود به برنامه",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'persianBold',
+                                fontSize: 18,
+                              ),
+                            ),
+                          ),
+                        )),
+                    SizedBox(
+                      height: 20,
                     ),
-                  ),
-                ],
-              ),
-            )
-          ],
+                    FadeAnimation(
+                        1.5,
+                        Text(
+                          "ثبت نام در برنامه",
+                          style: TextStyle(
+                            fontFamily: 'persianMedium',
+                            fontSize: 12,
+                            color: Color.fromRGBO(143, 148, 251, .6),
+                          ),
+                        )),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
