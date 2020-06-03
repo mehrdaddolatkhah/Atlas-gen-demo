9f45dc0a0472478e3ccfab5a09fef6aac67eaad7
Mehrdad <dolatkhah.mehrdad@gmail.com>
2020-06-03 09:42:27 +0430

    move login_screen into screen directory

--- lib/main.dart
+++ lib/main.dart
@@ -1,154 +1,38 @@
-import 'package:atlas_gen_demo/Animation/FadeAnimation.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import './screens/login_screen.dart';
 
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: MyApp(),
     ));
 
-class HomePage extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: Container(
-          child: Column(
-            children: <Widget>[
-              Container(
-                height: 400,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('assets/images/login.png'),
-                    fit: BoxFit.fill,
-                  ),
-                ),
+    return MaterialApp(
+      title: 'Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'persianMain',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'persianMedium',
               ),
-              Positioned(
-                child: FadeAnimation(
-                    1.8,
-                    Container(
-                      margin: EdgeInsets.only(top: 10),
-                      child: Center(
-                        child: Text(
-                          "ورود به برنامه",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            color: Color.fromRGBO(143, 148, 251, 1),
-                            fontSize: 30,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'persianBold',
-                          ),
-                        ),
-                      ),
-                    )),
-              ),
-              Padding(
-                padding: EdgeInsets.all(30.0),
-                child: Column(
-                  children: <Widget>[
-                    Container(
-                      padding: EdgeInsets.all(5),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10),
-                        boxShadow: [
-                          BoxShadow(
-                              color: Color.fromRGBO(143, 148, 251, .2),
-                              blurRadius: 20.0,
-                              offset: Offset(0, 10))
-                        ],
-                      ),
-                      child: Column(
-                        children: <Widget>[
-                          Container(
-                            padding: EdgeInsets.all(8.0),
-                            decoration: BoxDecoration(
-                                border: Border(
-                              bottom: BorderSide(color: Colors.grey[100]),
-                            )),
-                            child: TextField(
-                              textDirection: TextDirection.rtl,
-                              textAlign: TextAlign.right,
-                              decoration: InputDecoration(
-                                border: InputBorder.none,
-                                hintText: "09123456789",
-                                hintStyle: TextStyle(
-                                  color: Colors.grey[400],
-                                  fontFamily: 'persianMedium',
-                                  fontSize: 14,
-                                ),
-                              ),
-                            ),
-                          ),
-                          Container(
-                            padding: EdgeInsets.all(8.0),
-                            child: TextField(
-                              textAlign: TextAlign.right,
-                              textDirection: TextDirection.rtl,
-                              decoration: InputDecoration(
-                                border: InputBorder.none,
-                                hintText: "کلمه عبور",
-                                hintStyle: TextStyle(
-                                  color: Colors.grey[400],
-                                  fontFamily: 'persianMedium',
-                                  fontSize: 14,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    FadeAnimation(
-                        2,
-                        Container(
-                          height: 50,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            gradient: LinearGradient(
-                              colors: [
-                                Color.fromRGBO(143, 148, 251, .4),
-                                Color.fromRGBO(143, 148, 251, .8),
-                              ],
-                            ),
-                          ),
-                          child: Center(
-                            child: Text(
-                              "ورود به برنامه",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontFamily: 'persianBold',
-                                fontSize: 18,
-                              ),
-                            ),
-                          ),
-                        )),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    FadeAnimation(
-                        1.5,
-                        Text(
-                          "ثبت نام در برنامه",
-                          style: TextStyle(
-                            fontFamily: 'persianMedium',
-                            fontSize: 12,
-                            color: Color.fromRGBO(143, 148, 251, .6),
-                          ),
-                        )),
-                  ],
-                ),
-              )
-            ],
-          ),
-        ),
+            ),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      initialRoute: '/',
+      routes: {
+        '/': (ctx) => LoginPage(),
+      },
     );
   }
 }
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+import 'package:atlas_gen_demo/Animation/FadeAnimation.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 250,
+                margin: EdgeInsets.only(top: 50),
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/login.png'),
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+              Positioned(
+                child: FadeAnimation(
+                    1.8,
+                    Container(
+                      margin: EdgeInsets.only(top: 10),
+                      child: Center(
+                        child: Text(
+                          "ورود به برنامه",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Color.fromRGBO(143, 148, 251, 1),
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'persianBold',
+                          ),
+                        ),
+                      ),
+                    )),
+              ),
+              Padding(
+                padding: EdgeInsets.all(30.0),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Color.fromRGBO(143, 148, 251, .2),
+                              blurRadius: 20.0,
+                              offset: Offset(0, 10))
+                        ],
+                      ),
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            padding: EdgeInsets.all(8.0),
+                            decoration: BoxDecoration(
+                                border: Border(
+                              bottom: BorderSide(color: Colors.grey[100]),
+                            )),
+                            child: TextField(
+                              textDirection: TextDirection.rtl,
+                              textAlign: TextAlign.right,
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: "09123456789",
+                                hintStyle: TextStyle(
+                                  color: Colors.grey[400],
+                                  fontFamily: 'persianMedium',
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.all(8.0),
+                            child: TextField(
+                              textAlign: TextAlign.right,
+                              textDirection: TextDirection.rtl,
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: "کلمه عبور",
+                                hintStyle: TextStyle(
+                                  color: Colors.grey[400],
+                                  fontFamily: 'persianMedium',
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    FadeAnimation(
+                        2,
+                        Container(
+                          height: 50,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            gradient: LinearGradient(
+                              colors: [
+                                Color.fromRGBO(143, 148, 251, .4),
+                                Color.fromRGBO(143, 148, 251, .8),
+                              ],
+                            ),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "ورود به برنامه",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'persianBold',
+                                fontSize: 18,
+                              ),
+                            ),
+                          ),
+                        )),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    FadeAnimation(
+                        1.5,
+                        Text(
+                          "ثبت نام در برنامه",
+                          style: TextStyle(
+                            fontFamily: 'persianMedium',
+                            fontSize: 12,
+                            color: Color.fromRGBO(143, 148, 251, .6),
+                          ),
+                        )),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
